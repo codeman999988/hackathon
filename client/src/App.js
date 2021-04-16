03e4c03e6f4503dd301cd3758700fdf357f77dc6
orlandollerena <trancetribal@bellsouth.net>
2021-04-16 10:18:36 -0400

    two of the three graphs

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import './App.scss';
-import {BrowserRouter as Route, Link, Switch} from 'react-router-dom';
-import axios from 'axios';
-import React from 'react';
-import Header from '../components/Header/Header.jsx';
-import Employee from '../components/Employee/Employee.jsx';
-import Hours from '../components/Hours/Hours.jsx';
-import LineGraph from '../components/LineGraph/LineGraph.jsx';
-import PieChart from '../components/PieChart/PieChart.jsx';
-import Projects from '../components/Projects/Projects.jsx';
-import SideBar from '../components/SideBar/SideBar.jsx';
-import TopPerformer from '../components/TopPerformer/TopPerformer.jsx';
-import TreeMap from '../components/TreeMap/TreeMap.jsx';
-import TreeModal from '../components/TreeModal/TreeModal.jsx';
-
-
+import "./App.scss";
+// import {BrowserRouter as Route, Link, Switch} from 'react-router-dom';
+// import axios from 'axios';
+import React from "react";
+// import Header from '../components/Header/Header.jsx';
+// import Employee from '../components/Employee/Employee.jsx';
+// import Hours from '../components/Hours/Hours.jsx';
+import LineGraph from "../src/components/Line Graph/LineGraph.jsx";
+// import PieChart from '../components/PieChart/PieChart.jsx';
+// import Projects from '../components/Projects/Projects.jsx';
+// import SideBar from '../components/SideBar/SideBar.jsx';
+// import TopPerformer from '../components/TopPerformer/TopPerformer.jsx';
+import TreeMap from "./components/TreeMap/TreeMap.jsx";
+// import TreeModal from '../components/TreeModal/TreeModal.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <LineGraph />
+      <TreeMap />
     </div>
   );
 }
