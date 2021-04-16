255c2ee83201157664fed2c01932b3f3d0307ef6
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-04-15 22:20:17 -0400

    Adjusted tags to fit employee component

--- client/components/Employee/Employee.jsx
+++ client/components/Employee/Employee.jsx
@@ -5,13 +5,12 @@ export default class Employee extends Component {
         return (
             <div>
                 <div className='Emp__top-container'>
-                    <h2>Total Projects</h2>
+                    <h2>Total Editors</h2>
                     <p>...</p>
                 </div>
                 <div className='Emp__bot-container'>
                     {/* FILL VALUE WITH TOTAL NUMBER OF PROJECTS HANDED DOWN AS A PROP */}
-                    <h1>154</h1>
-
+                    <h1>324</h1>
                 </div>
             </div>
         )
