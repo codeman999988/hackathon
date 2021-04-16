16d055cf4045613748b57b5a2601b9aad2ea1d5e
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-04-16 11:59:39 -0400

    top performer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import './App.scss';
 import {BrowserRouter as Route, Link, Switch} from 'react-router-dom';
 import axios from 'axios';
 import React from 'react';
-import Header from './components/Header/Header.jsx';
-import Employee from './components/Employee/Employee.jsx';
-import Hours from './components/Hours/Hours.jsx';
-import LineGraph from './components/LineGraph/LineGraph.jsx';
-import PieChart from './components/PieChart/PieChart.jsx';
-import Projects from './components/Projects/Projects.jsx';
-import SideBar from './components/SideBar/SideBar.jsx';
+// import Header from './components/Header/Header.jsx';
+// import Employee from './components/Employee/Employee.jsx';
+// import Hours from './components/Hours/Hours.jsx';
+// import LineGraph from './components/LineGraph/LineGraph.jsx';
+// import PieChart from './components/PieChart/PieChart.jsx';
+// import Projects from './components/Projects/Projects.jsx';
+// import SideBar from './components/SideBar/SideBar.jsx';
 import TopPerformer from './components/TopPerformer/TopPerformer.jsx';
-import TreeMap from './components/TreeMap/TreeMap.jsx';
-import TreeModal from './components/TreeModal/TreeModal.jsx';
+// import TreeMap from './components/TreeMap/TreeMap.jsx';
+// import TreeModal from './components/TreeModal/TreeModal.jsx';
 
 
 
@@ -19,18 +19,18 @@ import TreeModal from './components/TreeModal/TreeModal.jsx';
 function App() {
   return (
     <div className="App">
-      <SideBar />
-      <Header />
+      {/* <SideBar />
+      <Header /> */}
       <section className="App__top-section">
-        <Projects />
+        {/* <Projects />
         <Employee />
-        <Hours />
+        <Hours /> */}
       </section>
       <section className="App__bot-section">
-        <LineGraph />
+        {/* <LineGraph /> */}
         <div className="App__bot-right-section">
           <TopPerformer />
-          <TreeMap />
+          {/* <TreeMap /> */}
         </div>
       </section>
     </div>
