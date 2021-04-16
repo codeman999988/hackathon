55454aa3fcfc1281b08065564f2e9c57fa43a19f
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-04-15 22:16:58 -0400

    JSX outline of Employee Component

---
+++ client/components/Employee/Employee.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Employee extends Component {
+    render() {
+        return (
+            <div>
+                <div className='Emp__top-container'>
+                    <h2>Total Projects</h2>
+                    <p>...</p>
+                </div>
+                <div className='Emp__bot-container'>
+                    {/* FILL VALUE WITH TOTAL NUMBER OF PROJECTS HANDED DOWN AS A PROP */}
+                    <h1>154</h1>
+
+                </div>
+            </div>
+        )
+    }
+}
