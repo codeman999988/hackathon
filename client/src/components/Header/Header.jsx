097d0900c9a3b0f4d533ec6ea061fbf275556d64
ashmarieg <ashleymglenny@gmail.com>
2021-04-15 23:50:41 -0400

    commit header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.scss'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Overview</h1>
+      <input className="header__search" type="text" placeholder="Search"></input>
+      <select className="header__months">
+      <option>April</option>
+      <option>May</option>
+      <option>June</option>
+      <option>July</option>
+      <option>August</option>
+      <option>September</option>
+      </select>
+
+      <select className="header__years">
+      <option>2021</option>
+      <option>2022</option>
+      <option>2023</option>
+      <option>2024</option>
+      <option>2025</option>
+      <option>2026</option>
+      </select>
+      <div className="header__placeholder"></div>
+      {/* <img></img>     */}
+    </div>
+  )
+}
+
+
+
