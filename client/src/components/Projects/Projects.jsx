7b564f528186872e1f54afbde6645b81022df319
Cody Crenshaw <codycrenshaw@Codys-MacBook-Pro.local>
2021-04-15 22:39:18 -0400

    Add scss and flex to projects

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -4,11 +4,11 @@ export default class Projects extends Component {
     render() {
         return (
             <div>
-                <div className='Emp__top-container'>
+                <div className='Projects__top-container'>
                     <h2>Total Projects</h2>
                     <p>...</p>
                 </div>
-                <div className='Emp__bot-container'>
+                <div className='Projects__bot-container'>
                     {/* FILL VALUE WITH TOTAL NUMBER OF PROJECTS HANDED DOWN AS A PROP */}
                     <h1>154</h1>
                 </div>
